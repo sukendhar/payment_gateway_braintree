b65758435bba759f5cff7bd4b62bea22cf90b500
sukendhar <sukendhar555@gmail.com>
2012-09-03 19:54:41 +0530

    payment gateway

---
+++ config/initializers/braintree.rb
@@ -0,0 +1,4 @@
+Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.merchant_id = "qyf39jcdmjx5gntz"
+Braintree::Configuration.public_key = "gth2q43tpj57cfxx"
+Braintree::Configuration.private_key = "9e3d675f6e491b4d225187212027f490"
